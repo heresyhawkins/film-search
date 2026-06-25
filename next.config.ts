2053da049cb684d543a4d5ae45bcbcfc15926906
heresyhawkins <heresyhawkins@gmail.com>
2026-06-25 18:38:12 +0300

    feat: delete unnecessary text

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "tvmaze.com", // на всякий случай
+        hostname: "tvmaze.com",
       },
     ],
   },
